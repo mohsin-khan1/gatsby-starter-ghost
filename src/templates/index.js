2eb3b94b1b5e667b5c29d1f4ae0d89ac69978828
Mohsin-khan1 <mohsinkhan.work@gmail.com>
2020-10-07 16:54:26 -0400

    edit header

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
-import { Helmet } from 'react-helmet'
 
 import { Layout, PostCard, Pagination } from '../components/common'
 import { MetaData } from '../components/common/meta'
@@ -41,9 +40,6 @@ const Index = ({ data, location, pageContext }) => {
                 location={location}
                 type="website"
             />
-            <Helmet>
-                <style type="text/css">{`${page.codeinjection_styles}`}</style>
-            </Helmet>
             <Layout>
                 <div className="container">
                     <article className="content">
