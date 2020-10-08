c481ca7b717f06be54b3235e3e39e5fef9694136
Mohsin-khan1 <mohsinkhan.work@gmail.com>
2020-10-08 10:19:27 -0400

    Add header files

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
 import { Link, StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
+import Header from 'header.js'
 
 import { Navigation } from '.'
 import config from '../../utils/siteConfig'
@@ -39,113 +40,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                 <div className="viewport-top">
                     {/* The main header section on top of the screen */}
 
-                    <div data-collapse="medium" data-animation="default" data-duration="400" role="banner" class="c-nav w-nav">
-  <div class="h-container c-nav__inner">
-    <div class="c-nav__hamburger-button w-nav-button">
-      <img src="images/hamburger.svg" alt="" class="h-svg-icon c-nav__icon--hamburger" />
-      <img src="images/times.svg" alt="" class="h-svg-icon c-nav__icon--times" />
-    </div>
-
-    <a href="/" aria-current="page" class="c-nav__logo w-nav-brand _w--current">
-      <img src={site.logo} alt={site.title} class="image" />
-    </a>
-
-    <nav role="navigation" class="c-nav__menu w-nav-menu">
-      <a href="/product" class="c-nav__menu-item w-nav-link">
-        Product
-      </a>
-
-      <div data-hover="1" data-delay="0" class="c-nav__dropdown w-dropdown">
-        <div class="c-nav__dropdown-toggle w-dropdown-toggle">
-          <a href="/industries" class="c-nav__menu-item w-nav-link">
-            Industries
-          </a>
-        </div>
-        <nav class="c-nav__dropdown-list w-dropdown-list">
-          <a href="/industries-small-business" class="c-nav__dropdown-item w-dropdown-link">
-            Small Business
-          </a>
-          <a href="/industries-real-estate" class="c-nav__dropdown-item w-dropdown-link">
-            Real Estate
-          </a>
-          <a href="/industries-legal" class="c-nav__dropdown-item w-dropdown-link">
-            Legal
-          </a>
-          <a href="/industries-government" class="c-nav__dropdown-item w-dropdown-link">
-            Government
-          </a>
-        </nav>
-      </div>
-
-      <a href="/pricing" class="c-nav__menu-item w-nav-link">
-        Pricing
-      </a>
-
-      <div data-hover="1" data-delay="0" class="c-nav__dropdown w-dropdown">
-        <div class="c-nav__dropdown-toggle w-dropdown-toggle">
-          <a href="/resources" class="c-nav__menu-item w-nav-link">
-            Resources
-          </a>
-        </div>
-        <nav class="c-nav__dropdown-list w-dropdown-list">
-          <a href="/faq" class="c-nav__dropdown-item w-dropdown-link">
-            FAQ
-          </a>
-          <a href="/training-and-tutorials" class="c-nav__dropdown-item w-dropdown-link h-stage-one--hide">
-            Training & Tutorials
-          </a>
-          <a href="/customer-service" class="c-nav__dropdown-item w-dropdown-link">
-            Customer Service
-          </a>
-          <a href="/partner-program" class="c-nav__dropdown-item w-dropdown-link">
-            Partner Program
-          </a>
-          <a href="/developer-program" class="c-nav__dropdown-item w-dropdown-link h-stage-one--hide">
-            Developer Program
-          </a>
-          <a href="/legal-terms-and-conditions" class="c-nav__dropdown-item w-dropdown-link">
-            Terms and Conditions
-          </a>
-        </nav>
-      </div>
-      <div data-hover="1" data-delay="0" class="c-nav__dropdown w-dropdown">
-        <div class="c-nav__dropdown-toggle w-dropdown-toggle">
-          <a href="/company" class="c-nav__menu-item w-nav-link">
-            Company
-          </a>
-        </div>
-        <nav class="c-nav__dropdown-list w-dropdown-list">
-          <a href="/about-us" class="c-nav__dropdown-item w-dropdown-link">
-            About Us
-          </a>
-          <a href="/news" class="c-nav__dropdown-item w-dropdown-link h-stage-one--hide">
-            News / Press / Blog
-          </a>
-          <a href="/media-relations" class="c-nav__dropdown-item w-dropdown-link h-stage-one--hide">
-            Media Relations
-          </a>
-          <a href="/investor-relations" class="c-nav__dropdown-item w-dropdown-link h-stage-one--hide">
-            Investor Relations
-          </a>
-          <a href="/careers" class="c-nav__dropdown-item w-dropdown-link h-stage-one--hide">
-            Careers
-          </a>
-        </nav>
-      </div>
-    </nav>
-
-    <div class="c-nav__buttons">
-      <a href="//app.docuwalk.com/login" class="c-nav__button c-nav__sign-in-button w-button">
-        Sign In
-      </a>
-      <a href="/pricing#plans" class="c-nav__button c-nav__sign-up-button w-button">
-       Get Started
-      </a>
-    </div>
-  </div>
-</div>
-
-
+                    {Header}
 
                    {/* <header className="site-head" style={{ ...site.cover_image && { backgroundImage: `url(${site.cover_image})` } }}>
                         <div className="container">
@@ -188,6 +83,26 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     </main>
 
                 </div>
+
+            <footer class="c-footer">
+              <div class="c-footer__body">
+                <div class="c-footer__body-inner h-container">
+                  {{> "footer/footer-contacts-block"}}
+
+                  {{> "footer/footer-nav-block"}}
+
+                  {{> "footer/footer-subscription-block"}}
+                </div>
+              </div>
+
+              <div class="c-footer__botton">
+                <div class="h-container c-footer__bottom-inner">
+                  {{> "footer/footer-copywrite-block"}}
+
+                  {{> "footer/footer-social-block"}}
+                </div>
+              </div>
+            </footer>
 
                 <div className="viewport-bottom">
                     {/* The footer at the very bottom of the screen */}
---
+++ src/components/common/header.js
@@ -0,0 +1,107 @@
+<header class="l-header">
+  <div data-collapse="medium" data-animation="default" data-duration="400" role="banner" class="c-nav w-nav">
+    <div class="h-container c-nav__inner">
+      <div class="c-nav__hamburger-button w-nav-button">
+        <img src="images/hamburger.svg" alt="" class="h-svg-icon c-nav__icon--hamburger" />
+        <img src="images/times.svg" alt="" class="h-svg-icon c-nav__icon--times" />
+      </div>
+
+      <a href="/" aria-current="page" class="c-nav__logo w-nav-brand _w--current">
+        <img src={site.logo} alt={site.title} class="image" />
+      </a>
+
+      <nav role="navigation" class="c-nav__menu w-nav-menu">
+        <a href="/product" class="c-nav__menu-item w-nav-link">
+          Product
+        </a>
+
+        <div data-hover="1" data-delay="0" class="c-nav__dropdown w-dropdown">
+          <div class="c-nav__dropdown-toggle w-dropdown-toggle">
+            <a href="/industries" class="c-nav__menu-item w-nav-link">
+              Industries
+            </a>
+          </div>
+          <nav class="c-nav__dropdown-list w-dropdown-list">
+            <a href="/industries-small-business" class="c-nav__dropdown-item w-dropdown-link">
+              Small Business
+            </a>
+            <a href="/industries-real-estate" class="c-nav__dropdown-item w-dropdown-link">
+              Real Estate
+            </a>
+            <a href="/industries-legal" class="c-nav__dropdown-item w-dropdown-link">
+              Legal
+            </a>
+            <a href="/industries-government" class="c-nav__dropdown-item w-dropdown-link">
+              Government
+            </a>
+          </nav>
+        </div>
+
+        <a href="/pricing" class="c-nav__menu-item w-nav-link">
+          Pricing
+        </a>
+
+        <div data-hover="1" data-delay="0" class="c-nav__dropdown w-dropdown">
+          <div class="c-nav__dropdown-toggle w-dropdown-toggle">
+            <a href="/resources" class="c-nav__menu-item w-nav-link">
+              Resources
+            </a>
+          </div>
+          <nav class="c-nav__dropdown-list w-dropdown-list">
+            <a href="/faq" class="c-nav__dropdown-item w-dropdown-link">
+              FAQ
+            </a>
+            <a href="/training-and-tutorials" class="c-nav__dropdown-item w-dropdown-link h-stage-one--hide">
+              Training & Tutorials
+            </a>
+            <a href="/customer-service" class="c-nav__dropdown-item w-dropdown-link">
+              Customer Service
+            </a>
+            <a href="/partner-program" class="c-nav__dropdown-item w-dropdown-link">
+              Partner Program
+            </a>
+            <a href="/developer-program" class="c-nav__dropdown-item w-dropdown-link h-stage-one--hide">
+              Developer Program
+            </a>
+            <a href="/legal-terms-and-conditions" class="c-nav__dropdown-item w-dropdown-link">
+              Terms and Conditions
+            </a>
+          </nav>
+        </div>
+        <div data-hover="1" data-delay="0" class="c-nav__dropdown w-dropdown">
+          <div class="c-nav__dropdown-toggle w-dropdown-toggle">
+            <a href="/company" class="c-nav__menu-item w-nav-link">
+              Company
+            </a>
+          </div>
+          <nav class="c-nav__dropdown-list w-dropdown-list">
+            <a href="/about-us" class="c-nav__dropdown-item w-dropdown-link">
+              About Us
+            </a>
+            <a href="/news" class="c-nav__dropdown-item w-dropdown-link h-stage-one--hide">
+              News / Press / Blog
+            </a>
+            <a href="/media-relations" class="c-nav__dropdown-item w-dropdown-link h-stage-one--hide">
+              Media Relations
+            </a>
+            <a href="/investor-relations" class="c-nav__dropdown-item w-dropdown-link h-stage-one--hide">
+              Investor Relations
+            </a>
+            <a href="/careers" class="c-nav__dropdown-item w-dropdown-link h-stage-one--hide">
+              Careers
+            </a>
+          </nav>
+        </div>
+      </nav>
+
+      <div class="c-nav__buttons">
+        <a href="//app.docuwalk.com/login" class="c-nav__button c-nav__sign-in-button w-button">
+          Sign In
+        </a>
+        <a href="/pricing#plans" class="c-nav__button c-nav__sign-up-button w-button">
+        Get Started
+        </a>
+      </div>
+    </div>
+  </div>
+</header>
