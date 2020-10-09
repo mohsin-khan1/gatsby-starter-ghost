b4b7a63570665f43c8f1cdefc2a31ed516fbd762
Mohsin-khan1 <mohsinkhan.work@gmail.com>
2020-10-09 15:18:07 -0400

    edit home page

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -34,7 +34,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                 <style type="text/css">{`${site.codeinjection_styles}`}</style>
                 <body className={bodyClass} />
             </Helmet>
-            <script src="../static/bundle.js"></script>
+            <script src="../bundle.js"></script>
 
 
             <div className="viewport">
