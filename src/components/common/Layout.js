fc8f5812f9c116727e7108771e02520629e77088
Mohsin-khan1 <mohsinkhan.work@gmail.com>
2020-10-12 13:30:05 -0400

    fixes nav bar

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -31,7 +31,8 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                 <html lang={site.lang} />
                 <style type="text/css">{`${site.codeinjection_styles}`}</style>
                 <body className="l-body"  />
-                <script src={withPrefix('./styles/bundle.js')} type="text/javascript"/>
+                {/* <script src={withPrefix('./styles/bundle.js')} type="text/javascript"/> */}
+                <script src="./styles/bundle.js"></script>
             </Helmet>
             <div className="viewport l-app">
                 <div className="viewport-top">
