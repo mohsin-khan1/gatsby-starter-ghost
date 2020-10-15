5a8e230905c01c657beccf872160f2b633841581
Mohsin-khan1 <mohsinkhan.work@gmail.com>
2020-10-15 11:54:52 -0400

    js fixes

--- src/templates/index.js
+++ src/templates/index.js
@@ -20,10 +20,6 @@ const Index = ({ data, location, pageContext }) => {
     return (
         <>
             <MetaData location={location} />
-            <Helmet>
-                {/* <style type="text/css">{`${page.codeinjection_styles}`}</style> */}
-                <script src="/bundle.js" async></script>
-            </Helmet>
             <Layout isHome={true}>
                 <div class="container">
                     <section class="post-feed">
