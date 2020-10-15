83a3a9156513805417636d624ae3ea8999381981
Mohsin-khan1 <mohsinkhan.work@gmail.com>
2020-10-15 14:09:52 -0400

    js fixes

--- static/custom_js.js
+++ static/custom_js.js
@@ -1,5 +1,6 @@
 function myFunction() {
     var x = document.getElementById("myDIV");
+    console.log('here');
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
